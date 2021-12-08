a54f9c9cd3c5b5e64437a57e942a1a28c30fd0c6
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-12-08 17:21:56 +0200

    Weather Observation Station 5 description

--- weather-observation-station-5.sql
+++ weather-observation-station-5.sql
@@ -1,5 +1,5 @@
 /*
-Enter your query here.
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
 */
 (SELECT CITY, LENGTH(CITY) FROM STATION WHERE LENGTH(CITY) = (SELECT LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) LIMIT 1) ORDER BY CITY LIMIT 1) 
 UNION (SELECT CITY, LENGTH(CITY) FROM STATION WHERE LENGTH(CITY) = (SELECT LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC LIMIT 1) ORDER BY CITY LIMIT 1);
